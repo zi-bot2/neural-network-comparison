2cd7a568e0758a40132c0f6a3d6003ef5b444447
ZiBook47 <zchua98@gmail.com>
2023-12-16 13:04:58 +0000

    Added CNN generalisation plots for master talk

--- dqnn_generalisation.py
+++ dqnn_generalisation.py
@@ -47,33 +47,6 @@ import os
 train_df.to_csv(f'/home/zchua/thesis_code/csvs/dqnn_train_df.csv')
 test_df.to_csv(f'/home/zchua/thesis_code/csvs/dqnn_test_df.csv')
 
-
-# sizeQuantumData = 2
-# dqnn = randomNetwork(qnnArch, sizeQuantumData + sizeTestData)
-# # trainOnSubset(dqnn[0], dqnn[1], dqnn[2], sizeQuantumData, sizeTestData, 1.5, learningRate, numEpochs)
-
-# plotlist, testPlotlist, currentUnitaries = qnnTrainingTesting(dqnn[0], dqnn[1], dqnn[2], sizeQuantumData, sizeTestData, 1, epsilon, numEpochs)
-
-# plotlist[1]
-
-# training_x = list(train_df)[1:]
-# training_y = [train_df[f'{sizeQuantumData}'].mean() for sizeQuantumData in training_x]
-# training_stds = [train_df[f'{sizeQuantumData}'].std() for sizeQuantumData in training_x]
-
-# testing_x = list(test_df)[1:]
-# testing_y = [test_df[f'{sizeQuantumData}'].mean() for sizeQuantumData in training_x]
-# testing_stds = [test_df[f'{sizeQuantumData}'].std() for sizeQuantumData in training_x]
-
-# plt.scatter(testing_x, testing_y, label='test')
-# plt.errorbar(testing_x, testing_y, yerr=testing_stds, fmt = 'o')
-# plt.scatter(training_x, training_y, label='train')
-# plt.errorbar(training_x, training_y, yerr=training_stds, fmt = 'o')
-# plt.xlabel('sizeQuantumData')
-# plt.ylabel('Fidelity')
-# plt.title(f'Fidelity for 4-8-4 CNN (1-2-1 DQNN) after {numEpochs} epochs\nLoss = {loss_fn}')
-# plt.legend()
-
-
 """## Kerstin's version"""
 
 # subsetNetwork22 = randomNetwork([1,2,1], 10)
